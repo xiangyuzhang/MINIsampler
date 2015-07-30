f0fb776615b657f955eeb102e743ad4c3ac9e059
xiangyuzhang <xiangyuzhang365@gmail.com>
2015-07-30 11:57:09 -0400

    feat: freq_divider is finally done

--- freq_divider.v
+++ freq_divider.v
@@ -1,5 +1,19 @@
-module freq_divider();
-	input
-	output
+module freq_divider(clk, clk_local, rst_n);
+
+	//frequency divider, here we make 4MHz clock to local time, we do this by a counter
+	input clk; 
+	input rst_n;
+	output clk_local;
+	
+	reg[6:0] cnt;
+	
+	always @(negedge clk or negedge rst_n)
+		if(!rst_n) cnt <= 7'd1;
+		else if(cnt < 7'd127) cnt <= cnt + 1'd1;
+		else cnt <= 7'd1;
+	
+	assign  clk_local = ( cnt <= 7'd63) ? 1'b0 : 1'b1;
+		
+	
 	
 endmodule 
