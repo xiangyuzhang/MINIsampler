3d8d2e364760df5d48c30b6e1fca5011b9ee4210
xiangyuzhang <xiangyuzhang365@gmail.com>
2015-07-30 12:23:33 -0400

    feat: add flag to localize clk
    What: flag = 1. then start to clk. flag = 0, stop

--- freq_divider.v
+++ freq_divider.v
@@ -1,18 +1,19 @@
-module freq_divider(clk, clk_local, rst_n);
+module freq_divider(clk, clk_local, rst_n, flag);
 
-	//frequency divider, here we make 4MHz clock to local time, we do this by a counter
-	input clk; 
+	//frequency divider, here we make 4MHz clock to local time, we do this by a counter, and localize clk
+	input clk,flag;  // flag is indicated the signal start(1) or signal stop(0)
 	input rst_n;
 	output clk_local;
 	
-	reg[6:0] cnt;
+	reg[7:0] cnt;
 	
-	always @(negedge clk or negedge rst_n)
-		if(!rst_n) cnt <= 7'd1;
-		else if(cnt < 7'd127) cnt <= cnt + 1'd1;
-		else cnt <= 7'd1;
+	always @(negedge clk or negedge rst_n or negedge flag)
+		if(!rst_n) cnt <= 8'd0;
+		else if (!flag) cnt <= 8'd0;
+		else if(cnt <= 8'd127) cnt <= cnt + 1'd1;
+		else cnt <= 8'd0;
 	
-	assign  clk_local = ( cnt <= 7'd63) ? 1'b0 : 1'b1;
+	assign  clk_local = ( cnt <= 8'd64) ? 1'b0 : 1'b1;
 		
 	
 	
